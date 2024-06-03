7c9d23f2ab9dd66dc5602eedbee4a44dce6b815a
narendra <narendranishad59@gmail.com>
2024-06-03 14:47:19 +0530

    Final

---
+++ src/Pages/NotFound/Notfound.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Notfound() {
+  return (
+    <>
+    
+    <div className="h-screen w-full flex justify-center items-center">
+        <h1 className="text-xl font-bold">404</h1>
+        <p className="text-lg font-serif">Page not found</p>
+    </div>
+    
+    </>
+  )
+}
---
+++ src/context/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBZA_xEwHpwtMe4LmUhxsOHoNrfxUEOn8Q",
+    authDomain: "trendluxe-ac615.firebaseapp.com",
+    projectId: "trendluxe-ac615",
+    storageBucket: "trendluxe-ac615.appspot.com",
+    messagingSenderId: "858991018854",
+    appId: "1:858991018854:web:8db22caadda2d231557ea7"
+  
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+
